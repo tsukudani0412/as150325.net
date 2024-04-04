942fb26f7440d0bc0454992e2527b405123ef415
Tsukudani <me@tkdn.jp>
2024-04-05 01:04:19 +0900

    fix some

--- ipver.php
+++ ipver.php
@@ -3,11 +3,8 @@ function returnIPver($ip) {
 	if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
 		return 4;
 	}
-	else if(filter_var($ip, FILTER_VALIDATE_IP, FLITER_FLAG_IPV6)) {
-		return 6;
-	}
 	else {
-		return false;
+		return 6;
 	}
 }
 
